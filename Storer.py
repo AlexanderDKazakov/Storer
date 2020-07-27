2ebb39f2b016e238abd94296ddaba17690907782
Alexander Kazakov <alexander.d.kazakov@gmail.com>
2020-07-27 11:01:49 +0200

    load --> return information if needed; show has a flag  for receiving string representation

--- Storer.py
+++ Storer.py
@@ -3,32 +3,32 @@ import pickle
 
 class Storer:
     def __init__(self, name4file=None, path4dumbs=None, internal_name=None ):
-        __version__ = "0.0.2"
+        __version__ = "0.0.3"
         self.internal_name = "[Storer]"
         self.name4file = "noname.pkl"
         self.path4dumbs = os.path.expanduser(os.path.dirname(__file__))
         self.data = dict()
-        
+
         if internal_name: self.internal_name = "[" + str(internal_name) + "]"
         if name4file: self.name4file = str(name4file)
-        if path4dumbs: 
+        if path4dumbs:
             self.path4dumbs = str(path4dumbs)
             if self.path4dumbs[-1] != "/": self.path4dumbs += "/"
             self.path4dumbs = os.path.expanduser(self.path4dumbs)
-            os.makedirs(self.path4dumbs, exist_ok=True)        
-        
+            os.makedirs(self.path4dumbs, exist_ok=True)
+
     def put(self, what=None, name: str = None) -> None:
         self.data[name] = what
 
     def get(self, name: str = None):
         if name in self.data: return self.data[name]
         else: return False
-    
+
     def dumb(self):
         print(self.internal_name, self.path4dumbs + self.name4file + " dumping...")
         with open(self.path4dumbs + self.name4file, 'wb') as f:
             pickle.dump(self.data, f)
-    
+
     def load(self):
         print(self.internal_name, self.path4dumbs + self.name4file + " loading...")
         if os.path.exists(self.path4dumbs + self.name4file):
@@ -36,11 +36,14 @@ class Storer:
                      self.data = pickle.load(f)
         else:
             print(self.internal_name, "No data is available for loading...")
-    
-    def show(self):
-        print(self.internal_name, "You have:")
+        return self.data
+
+    def show(self, get_string = False):
+        string = ""
         for name in self.data:
-            print("key: {0:10} | value:  {1:8}".format(name, str(self.data[name])))             
+            string += "key: {0:10} | value:  {1:4}; ".format(name, str(self.data[name]))
+        if get_string: return string
+        else: print(string)
 
 if __name__ == "__main__":
     s = Storer(path4dumbs='./test')
@@ -50,9 +53,9 @@ if __name__ == "__main__":
     s.put(what={v:v*2 for v in range(10)}, name="mydict")
     s.show()
     s.dumb()
-    
+
     print("\n Now we creating new storer instance...\n")
     s1 = Storer(internal_name="Storer1", path4dumbs='./test')
     s1.load()
     s1.show()
-    
+
