438ad89f9169d3f485214ff497a1b08cf678fbab
Alexander <alexander.d.kazakov@gmail.com>
2020-11-17 10:58:27 +0100

    package preparations3

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Storer',
   packages = ['Storer'],
-  version = '0.4',
+  version = '0.5',
   license='MIT',
   description = 'Minimalist storage class for any purpose.',
   author = 'Alexander Kazakov',
   author_email = 'alexander.d.kazakov@gmail.com',
   url = 'https://github.com/AlexanderDKazakov/Storer',
-  download_url = 'https://github.com/AlexanderDKazakov/Storer/archive/v0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/AlexanderDKazakov/Storer/archive/v0.5.tar.gz',    # I explain this later on
   keywords = ['store', 'pickle'],
   install_requires=[
           'pickle==4.0',
